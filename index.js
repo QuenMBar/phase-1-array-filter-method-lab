11793e2752c425e9a9bcbf34d9996485bc4818c8
QuenMBar <Qbeast360@gmail.com>
2021-02-23 15:42:06 -0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solution here
+function findMatching(drivers, name) {
+    return drivers.filter(driverName => driverName.toUpperCase() === name.toUpperCase());
+}
+function fuzzyMatch(drivers, name) {
+    return drivers.filter(driverName => driverName.toUpperCase()[0] === name.toUpperCase()[0]);
+}
+function matchName(drivers, name) {
+    return drivers.filter(driverInfo => driverInfo.name.toUpperCase() === name.toUpperCase());
+}
